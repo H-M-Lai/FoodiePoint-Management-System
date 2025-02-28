e7b9a8b9af9e5c00870e92e2c8f7f119ec1ae91f
Hoo021 <pcng1975@gmail.com>
2025-02-28 11:44:38 +0800

    1

---
+++ Customer/Customer_Feedback.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Customer
+{
+    partial class Customer_Feedback
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Customer_Feedback
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1182, 753);
+            this.Name = "Customer_Feedback";
+            this.Text = "Customer_Feedback";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Customer/Customer_Main.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Customer
+{
+    public partial class Customer_Main : Form
+    {
+        public Customer_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Customer_Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblcus_feedbacks_Click(object sender, EventArgs e)
+        {
+            Customer_Feedback customer_feedback = new Customer_Feedback();
+            customer_feedback.Show();
+        }
+
+        private void lblcus_profile_Click(object sender, EventArgs e)
+        {
+            Customer_Profile customer_profile = new Customer_Profile();
+            customer_profile.Show();
+        }
+
+        private void lblcus_reservations_Click(object sender, EventArgs e)
+        {
+            Customer_Reservation customer_reservation = new Customer_Reservation();
+            customer_reservation.Show();
+        }
+
+        private void lblcus_orderfood_Click(object sender, EventArgs e)
+        {
+            Customer_Order customer_order = new Customer_Order();
+            customer_order.Show();
+        }
+
+        private void lblcus_vieworders_Click(object sender, EventArgs e)
+        {
+            Customer_OrderStatus customer_orderstatus = new Customer_OrderStatus();
+            customer_orderstatus.Show();
+        }
+    }
+}
